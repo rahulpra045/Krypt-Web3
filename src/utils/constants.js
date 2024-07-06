00421509415fdde58cbb220e33d385c7535732b3
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2024-07-06 17:52:22 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xC7c97243419fB07945A9D64E2CfA5518D83c4753';
---
+++ src/utils/shortenAddress.js
@@ -0,0 +1,6 @@
+// const shortenAddress = undefined;
+// export const shortenAddress = (address) => `${address.slice(0, 5)}...${address.slice(address.length - 4)}`;
+
+// export const shortenAddress = (address) => `${address?.slice(0, 5)}...${address?.slice(address.length - 4)}`;
+
+export const shortenAddress = (address) => `${address.slice(0, 5)}...${address.slice(address.length - 4)}`
